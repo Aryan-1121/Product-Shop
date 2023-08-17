f627efd49b184cff54c242684af45488ee58d67b
Aryan <aryanmaurya879@gmail.com>
2023-08-17 17:00:38 +0530

    added model, dto, service, repo

--- product-service/src/main/java/com/productshop/productservice/main/ProductServiceApplication.java
+++ product-service/src/main/java/com/productshop/productservice/main/ProductServiceApplication.java
@@ -1,4 +1,4 @@
-package com.productshop.productservice;
+package com.productshop.productservice.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ product-service/src/main/java/com/productshop/productservice/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.productshop.productservice.service;
+
+import org.springframework.stereotype.Service;
+
+import com.productshop.productservice.dto.ProductRequest;
+import com.productshop.productservice.model.Product;
+
+@Service
+public class ProductService {
+
+	public void createProduct(ProductRequest productRequest) {
+//		Product product = 
+		
+	}
+}
