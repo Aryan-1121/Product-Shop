27972ce9f60c06f309108b4e048ea6d9a4cc7bc9
Aryan <aryanmaurya879@gmail.com>
2024-05-22 16:06:40 +0530

    standard API gateway revised

--- microservices-parent/microservices-new/api-gateway/src/main/java/com/productshop/apigateway/ApiGatewayApplication.java
+++ microservices-parent/microservices-new/api-gateway/src/main/java/com/productshop/apigateway/ApiGatewayApplication.java
@@ -4,12 +4,14 @@ import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
-    @Autowired
-    private EurekaClient eurekaClient;
+
+//    @Autowired
+//    private EurekaClient eurekaClient;
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class,args);
     }
--- microservices-parent/microservices-new/api-gateway/src/main/java/config/SecurityConfig.java
+++ microservices-parent/microservices-new/api-gateway/src/main/java/config/SecurityConfig.java
@@ -1,35 +1,37 @@
-package config;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        serverHttpSecurity
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange ->
-                        exchange.pathMatchers("/eureka/**")
-                                .permitAll()
-                                .anyExchange()
-                                .authenticated())
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-        return serverHttpSecurity.build();
-    }
-
-
+//package config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@Configuration
+//@EnableWebFluxSecurity          // Spring cloud API GW is based on spring webFlux not on spring mvc
+//public class SecurityConfig {
+//
+//    // since we are talking only with REST
+//    // excluding all eureka static  resources bcs we dont need to authenticate them
 //    @Bean
-//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-//        http
-//                // ...
-//                .csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()));
-//        return http.build();
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//        serverHttpSecurity
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchange ->
+//                        exchange.pathMatchers("/eureka/**")
+//                                .permitAll()
+//                                .anyExchange()
+//                                .authenticated())
+//                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+//        return serverHttpSecurity.build();
 //    }
-}
+//
+//
+////    @Bean
+////    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+////        http
+////                // ...
+////                .csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()));
+////        return http.build();
+////    }
+//}
