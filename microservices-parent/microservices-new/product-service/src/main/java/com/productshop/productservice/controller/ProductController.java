7daf60886ee556303ae5505fb9f7b77e22404b90
Aryan <aryanmaurya879@gmail.com>
2024-05-28 11:29:29 +0530

    able to fetch the current dynamic port on which service is runnint (will be used in prometheus.yml)

--- microservices-parent/microservices-new/product-service/src/main/java/com/productshop/productservice/controller/ProductController.java
+++ microservices-parent/microservices-new/product-service/src/main/java/com/productshop/productservice/controller/ProductController.java
@@ -2,6 +2,11 @@ package com.productshop.productservice.controller;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +25,13 @@ import lombok.RequiredArgsConstructor;
 @RestController					//@RestController (Controller + responseBody) since we are exposing rest api
 @RequestMapping("/api/product")			// default url prefix for this microservice
 @RequiredArgsConstructor				//for final ProductService
+@Slf4j
 public class ProductController {
-	
-	
+
+
+@Autowired
+private ServletWebServerApplicationContext webServerApplicationContext;
+
 	private final ProductService productService;
 	
 	@PostMapping
@@ -39,6 +48,7 @@ public class ProductController {
 	@ResponseStatus(HttpStatus.OK)
 //	ProductResponse will be same as Product -> including ID  bcs its the result from  DB and we will get everything from db, now its up to us to however use the output if required
 	public List<ProductResponse> getAllProducts(){
+		log.info("port is {}", webServerApplicationContext.getWebServer().getPort());
 		return productService.getAllProducts();
 	}
 }
